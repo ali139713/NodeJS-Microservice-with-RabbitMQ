8cb53138eea1d25fe4ba9224c261e28c71179b25
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-04-02 01:36:37 +0500

    Connected db in auth service.

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT_ONE || 7070;
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/auth-service", {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}, () => {
+    console.log('Auth-service DB connected.')
+})
 
 app.use(express.json())
 
