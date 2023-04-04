8c2410952a9d4ca21d10dec56917be41667ec0e5
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-04-04 23:31:18 +0500

    Implemented queue to push order and products in queue.

--- product-service/index.js
+++ product-service/index.js
@@ -24,13 +24,13 @@ mongoose.connect(
     }
 );
 
-// async function connect() {
-//     const amqpServer = "amqp://localhost:5672";
-//     connection = await amqp.connect(amqpServer);
-//     channel = await connection.createChannel();
-//     await channel.assertQueue("PRODUCT");
-// }
-// connect();
+async function connect() {
+    const amqpServer = "amqp://localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("PRODUCT");
+}
+connect();
 
 app.post("/product/buy", isAuthenticated, async (req, res) => {
     const { ids } = req.body;
